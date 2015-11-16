df9d4992e2d212f20a1a19b6c049880033b596e5
dirgepye <s.stenhouse87@gmail.com>
2015-11-16 21:02:03 +0000

    returning array  sorted by longest word

--- functions.js
+++ functions.js
@@ -1,1 +1,26 @@
 // Javascript part 2!
+
+//sorting an array by the longest word
+
+var strings = ["apu", "chief wiggum", "homer", "moleman"];
+
+
+function getLongestWord(arrayOfWord){
+   
+  return arrayOfWord.sort(
+    function (a,b) {
+      if (a.length > b.length) {
+        return -1;
+      }
+      else if (a.length < b.length) {
+        return 1;
+      }
+      else 
+      {
+        return 0;
+      }
+  });
+  
+}
+
+console.log(getLongestWord(strings));
